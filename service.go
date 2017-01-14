2059d6df9961908927aeea5974f10da82134bfd3
fabioswartz <ffcf.pt.en@gmail.com>
2017-01-14 16:39:49 +0000

    Caddy service

---
+++ service.go
@@ -0,0 +1,92 @@
+package service
+
+import (
+	"flag"
+	"os"
+
+	"github.com/kardianos/service"
+	"github.com/mholt/caddy"
+)
+
+var (
+	logger       service.Logger
+	name, action string
+)
+
+func init() {
+	flag.StringVar(&name, "name", "Caddy", "Caddy's service name")
+	flag.StringVar(&action, "service", "", "install, uninstall, start, stop, restart")
+
+	caddy.RegisterPlugin("service", caddy.Plugin{
+		StartupHook: hook,
+	})
+}
+
+type program struct{}
+
+func (p *program) Start(s service.Service) error {
+	// Get Caddyfile input
+	caddyfile, err := caddy.LoadCaddyfile(flag.Lookup("type").Value.String())
+	if err != nil {
+		return err
+	}
+
+	// Start your engines
+	_, err = caddy.Start(caddyfile)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *program) Stop(s service.Service) error {
+	return caddy.Stop()
+}
+
+func hook() error {
+	flags := []string{
+		"conf",
+		"type",
+		"log",
+		"http2",
+		"email",
+		"grace",
+		"cpu",
+	}
+
+	config := &service.Config{
+		Name:        name,
+		DisplayName: name,
+		Description: "Caddy's service",
+		Arguments:   []string{},
+	}
+
+	for k := range flags {
+		f := flag.Lookup(flags[k])
+		if f.Value.String() != f.DefValue {
+			config.Arguments = append(config.Arguments, "-"+flags[k], f.Value.String())
+		}
+	}
+
+	s, err := service.New(&program{}, config)
+	if err != nil {
+		return err
+	}
+
+	if action != "" {
+		err = service.Control(s, action)
+		if err != nil {
+			return err
+		}
+		os.Exit(0)
+	}
+
+	err = s.Run()
+	if err != nil {
+		return err
+	}
+
+	os.Exit(0)
+	return nil
+}
